f7809a79a29e383622b68dc46fead8e90074e67a
James <jamezbrown100@gmail.com>
2016-11-06 17:51:03 -0600

    Create goal cell class

---
+++ Goals/Assets.xcassets/GoalCell.swift
@@ -0,0 +1,16 @@
+//
+//  GoalCell.swift
+//  Goals
+//
+//  Created by James Brown on 11/6/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class GoalCell: UITableViewCell {
+
+    @IBOutlet weak var nameLbl: UILabel!
+    @IBOutlet weak var dateLbl: UILabel!
+    
+}
