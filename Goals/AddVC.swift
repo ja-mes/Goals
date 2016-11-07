a1b19eea59f0973e119d2c2ad9e9edb0e41f6e04
James <jamezbrown100@gmail.com>
2016-11-06 19:53:57 -0600

    Wire up back button and create add vc class

---
+++ Goals/AddVC.swift
@@ -0,0 +1,23 @@
+//
+//  AddVC.swift
+//  Goals
+//
+//  Created by James Brown on 11/6/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class AddVC: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+    @IBAction func backPressed(_ sender: Any) {
+        dismiss(animated: true, completion: nil)
+    }
+
+}
