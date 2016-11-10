94bcaf190c678a14c37684f609e98afe77ba46fc
James <jamezbrown100@gmail.com>
2016-11-10 11:09:06 -0600

    Work on setting up save function

--- Goals/AddVC.swift
+++ Goals/AddVC.swift
@@ -10,14 +10,44 @@ import UIKit
 
 class AddVC: UIViewController {
 
+    @IBOutlet weak var name: CustomField!
+    @IBOutlet weak var datePicker: UIDatePicker!
+    
+    
     override func viewDidLoad() {
         super.viewDidLoad()
 
-        // Do any additional setup after loading the view.
+        
     }
     
     @IBAction func backPressed(_ sender: Any) {
         dismiss(animated: true, completion: nil)
     }
 
+    @IBAction func savePressed(_ sender: UIButton) {
+        let goal = Goal(context: context)
+
+        if configure(goal: goal) {
+            goal.date = datePicker.date as NSDate?
+            goal.color = "exampleColor"
+            
+            appDel.saveContext()
+            
+            dismiss(animated: true, completion: nil)
+        } else {
+            if goal.objectID.isTemporaryID {
+                context.delete(goal)
+            }
+        }
+    }
+    
+    func configure(goal: Goal) -> Bool {
+        if let text = name.text, text.isEmpty == false {
+            goal.name = text
+            
+            return true
+        }
+        
+        return false
+    }
 }
