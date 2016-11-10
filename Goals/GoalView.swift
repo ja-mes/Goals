069722213314aaabc10ca2060dbcc262f23d73d6
James <jamezbrown100@gmail.com>
2016-11-09 18:33:36 -0600

    Modify spacing inbetween cells

---
+++ Goals/GoalView.swift
@@ -0,0 +1,16 @@
+//
+//  GoalView.swift
+//  Goals
+//
+//  Created by James Brown on 11/9/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class GoalView: UIView {
+
+    override func awakeFromNib() {
+        layer.cornerRadius = 10
+    }
+}
