cf22032003d3df4af79ea7c111d2ae62ff09001d
James <jamezbrown100@gmail.com>
2016-11-15 12:30:32 -0600

    Create rounded button class

--- Goals/RoundedButton.swift
+++ Goals/RoundedButton.swift
@@ -10,12 +10,7 @@ import UIKit
 
 class RoundedButton: UIButton {
 
-    /*
-    // Only override draw() if you perform custom drawing.
-    // An empty implementation adversely affects performance during animation.
-    override func draw(_ rect: CGRect) {
-        // Drawing code
+    override func awakeFromNib() {
+        layer.cornerRadius = 12
     }
-    */
-
 }
