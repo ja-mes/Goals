c0bedbe9d86c246a6a6cc35127aa343f1756641a
James <jamezbrown100@gmail.com>
2016-11-15 12:23:37 -0600

    Add buttons

---
+++ Goals/RoundedButton.swift
@@ -0,0 +1,21 @@
+//
+//  RoundedButton.swift
+//  Goals
+//
+//  Created by James Brown on 11/15/16.
+//  Copyright © 2016 James Brown. All rights reserved.
+//
+
+import UIKit
+
+class RoundedButton: UIButton {
+
+    /*
+    // Only override draw() if you perform custom drawing.
+    // An empty implementation adversely affects performance during animation.
+    override func draw(_ rect: CGRect) {
+        // Drawing code
+    }
+    */
+
+}
